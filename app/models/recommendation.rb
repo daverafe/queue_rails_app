27b46037fb2f05aec4b5e53e0c4560f5d501147a
David Rafe <david.rafe@gmail.com>
2021-05-04 19:51:41 -0400

    added validation to recommendation rating, must be a number between 1 and 10

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -4,6 +4,8 @@ class Recommendation < ApplicationRecord
     belongs_to :media_asset  
     accepts_nested_attributes_for :media_asset, reject_if: :all_blank 
 
+    validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
 
     def self.most_recent
       self.order(created_at: :desc)

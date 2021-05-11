aed485f3c4e61f89624745c40ab3e660309b59a4
David Rafe <david.rafe@gmail.com>
2021-05-11 19:56:38 -0400

    updated user model so that user can delete account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :received_recommendations, class_name: "Recommendation", foreign_key: "recommendation_receiver_id"
-  has_many :made_recommendations, class_name: "Recommendation", foreign_key: "recommendation_maker_id"
+  has_many :received_recommendations, class_name: "Recommendation", foreign_key: "recommendation_receiver_id", dependent: :destroy
+  has_many :made_recommendations, class_name: "Recommendation", foreign_key: "recommendation_maker_id", dependent: :destroy
   has_many :media_assets, through: :recommendations
 
   validates :username, :email, presence: true
